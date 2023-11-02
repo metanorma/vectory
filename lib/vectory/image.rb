c500ca4120bed42448b377c9551ea497a116e8fd
Alexey Morozov <alexey.morozov.is@gmail.com>
2023-11-03 00:35:56 +0100

    File system operations

--- lib/vectory/image.rb
+++ lib/vectory/image.rb
@@ -6,8 +6,7 @@ module Vectory
   class Image
     class << self
       def from_path(path)
-        content = File.read(path, mode: "rb")
-        new(content, path)
+        new(File.read(path, mode: "rb"))
       end
 
       def from_content(content)
@@ -15,41 +14,10 @@ module Vectory
       end
     end
 
-    attr_reader :content, :path
+    attr_reader :content
 
-    def initialize(content = nil, path = nil)
+    def initialize(content)
       @content = content
-      @path = path
-    end
-
-    def write(path)
-      File.binwrite(path, @content)
-      @path = File.expand_path(path)
-
-      self
-    end
-
-    def with_file(input_extension)
-      Dir.mktmpdir do |dir|
-        input_path = File.join(dir, "image.#{input_extension}")
-        File.binwrite(input_path, @content)
-
-        yield input_path
-      end
-    end
-
-    def save_dataimage(uri, _relative_dir = true)
-      %r{^data:(?<imgclass>image|application)/(?<imgtype>[^;]+);(?:charset=[^;]+;)?base64,(?<imgdata>.+)$} =~ uri
-      imgtype = "emf" if emf?("#{imgclass}/#{imgtype}")
-      imgtype = imgtype.sub(/\+[a-z0-9]+$/, "") # svg+xml
-      imgtype = "png" unless /^[a-z0-9]+$/.match? imgtype
-      imgtype == "postscript" and imgtype = "eps"
-      Tempfile.open(["image", ".#{imgtype}"]) do |f|
-        f.binmode
-        f.write(Base64.strict_decode64(imgdata))
-        @tempfile_cache << f # persist to the end
-        f.path
-      end
     end
   end
 end
--- lib/vectory/vector.rb
+++ lib/vectory/vector.rb
@@ -4,6 +4,11 @@ require_relative "inkscape_converter"
 
 module Vectory
   class Vector < Image
+    def self.from_path(path)
+      content = File.read(path, mode: "rb")
+      new(content, path)
+    end
+
     def self.default_extension
       raise Vectory::NotImplementedError,
             "#default_extension should be implemented in a subclass."
@@ -12,6 +17,14 @@ module Vectory
     def self.mimetype
       raise Vectory::NotImplementedError,
             "#mimetype should be implemented in a subclass."
+    end
+
+    attr_reader :initial_path
+
+    def initialize(content = nil, initial_path = nil)
+      super(content)
+
+      @initial_path = initial_path
     end
 
     def to_uri
@@ -27,6 +40,35 @@ module Vectory
 
         target_class.from_path(output_path)
       end
+    end
+
+    def write(path = nil)
+      target_path = path || @path || tmp_path
+      File.binwrite(target_path, @content)
+      @path = File.expand_path(target_path)
+
+      self
+    end
+
+    def path
+      @path || raise(NotWrittenToDiskError)
+    end
+
+    private
+
+    def with_file(input_extension)
+      Dir.mktmpdir do |dir|
+        input_path = File.join(dir, "image.#{input_extension}")
+        File.binwrite(input_path, @content)
+
+        yield input_path
+      end
+    end
+
+    def tmp_path
+      dir = Dir.mktmpdir
+      filename = "image.#{self.class.default_extension}"
+      File.join(dir, filename)
     end
   end
 end
